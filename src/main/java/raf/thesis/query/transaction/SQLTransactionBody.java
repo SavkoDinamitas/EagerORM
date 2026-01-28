d495b5b0bacce64401958d6df8669b1935aae375
DimitrijeA <53818425+SavkoDinamitas@users.noreply.github.com>
2026-01-28 15:33:44 +0100

    add transaction functional interfaces documentation

--- src/main/java/raf/thesis/query/transaction/SQLTransactionBody.java
+++ src/main/java/raf/thesis/query/transaction/SQLTransactionBody.java
@@ -3,6 +3,9 @@ package raf.thesis.query.transaction;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Functional interface for representing transaction bodies that doesn't have return value.
+ */
 @FunctionalInterface
 public interface SQLTransactionBody {
     void execute(Connection conn) throws SQLException;
--- src/main/java/raf/thesis/query/transaction/SQLValuedTransactionBody.java
+++ src/main/java/raf/thesis/query/transaction/SQLValuedTransactionBody.java
@@ -3,6 +3,9 @@ package raf.thesis.query.transaction;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Functional interface for representing transaction bodies that have return value.
+ */
 @FunctionalInterface
 public interface SQLValuedTransactionBody<T> {
     T execute(Connection conn) throws SQLException;
